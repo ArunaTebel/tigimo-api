4909de4236ad6a6dd5463a13076f957507e6831d
ArunaTebel <arunatebel@hotmail.com>
2022-07-11 01:24:34 +0530

    Refactor and add doc comments to posting service.

--- src/service/posting.js
+++ src/service/posting.js
@@ -3,68 +3,108 @@ const {ObjectID} = require('mongodb');
 
 const collectionName = 'postings';
 
+/**
+ * Returns all the Postings
+ *
+ * @returns {Promise}
+ */
 async function getPostings() {
     const database = await getDatabase();
-    const postings = await database.collection(collectionName).find({}).toArray();
-    await closeDatabase();
-    return postings;
+    return database
+        .collection(collectionName)
+        .find({})
+        .toArray();
 }
 
+/**
+ * Returns the Postings created by the given user
+ *
+ * @param userId - id of the user that created the returned Postings
+ * @returns {Promise<*>}
+ */
 async function getPostingsByUserId(userId) {
     const database = await getDatabase();
-    const postings = await database.collection(collectionName).find({userId: userId}).toArray();
-    await closeDatabase();
-    return postings;
+    return database
+        .collection(collectionName)
+        .find({userId: userId})
+        .toArray();
 }
 
+/**
+ * Inserts a new Posting
+ *
+ * @param postingData - Posting data.
+ * Eg: {title: 'My Posting', url: 'https://example.com', note: 'My Note'}
+ * @returns {Promise<*>}
+ */
 async function insertPosting(postingData) {
     const database = await getDatabase();
-    const {insertedId} = await database.collection(collectionName).insertOne(postingData);
-    await closeDatabase();
+    const {insertedId} = await database
+        .collection(collectionName)
+        .insertOne(postingData);
     return insertedId;
 }
 
+/**
+ * Updates the Posting having the given id
+ *
+ * @param id
+ * @param posting
+ * @returns {Promise<*>}
+ */
 async function updatePosting(id, posting) {
     const database = await getDatabase();
-    const postings = await database.collection(collectionName).update(
-        {_id: new ObjectID(id),},
-        {
-            $set: {
-                ...posting,
+    return database
+        .collection(collectionName)
+        .updateOne(
+            {_id: new ObjectID(id),},
+            {
+                $set: {
+                    ...posting,
+                },
             },
-        },
-    );
-    await closeDatabase();
-    return postings;
+        );
 }
 
+/**
+ * Returns the Posting having the given id
+ *
+ * @param id
+ * @returns {Promise<{}|*>}
+ */
 async function getPosting(id) {
     const database = await getDatabase();
     try {
-        return await database.collection(collectionName).findOne(
-            {_id: new ObjectID(id),}
-        );
+        return database
+            .collection(collectionName)
+            .findOne(
+                {_id: new ObjectID(id),}
+            );
     } catch (e) {
         return {}
-    } finally {
-        await closeDatabase();
     }
 }
 
+/**
+ * Deletes the Posting having the given id
+ *
+ * @param id
+ * @returns {Promise<*>}
+ */
 async function deletePosting(id) {
     const database = await getDatabase();
-    const postings = await database.collection(collectionName).deleteOne({
-        _id: new ObjectID(id),
-    });
-    await closeDatabase();
-    return postings;
+    return database
+        .collection(collectionName)
+        .deleteOne({
+            _id: new ObjectID(id),
+        });
 }
 
 module.exports = {
-    insertPosting,
     getPostings,
     getPosting,
+    getPostingsByUserId,
+    insertPosting,
     updatePosting,
     deletePosting,
-    getPostingsByUserId,
 };
